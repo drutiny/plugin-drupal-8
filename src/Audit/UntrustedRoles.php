df498851d835e075993650d08cb6e1f76f704ab7
Brian Schilt <brian.schilt@acquia.com>
2018-03-07 21:53:15 -0700

    refactored the code in order to reduce the external calls to 1

--- src/Audit/UntrustedRoles.php
+++ src/Audit/UntrustedRoles.php
@@ -10,44 +10,42 @@ use Drutiny\Sandbox\Sandbox;
  */
 class UntrustedRoles extends Audit {
 
-  /**
-   *
-   */
   public function audit(Sandbox $sandbox) {
-    // Get the machine names of the untrusted roles
-    $untrusted_roles = $sandbox->getParameter('untrusted_roles', ['anonymous', 'authenticated']);
-    
-    // Load all of Drupal's permissions so that we have access to the
-    // "restrict access" property.
-    $command = "echo json_encode(\\Drupal::service('user.permissions')->getPermissions());";
-    $command = "\"$command\"";
-    $all_permissions = $sandbox->drush(['format' => 'json'])->ev($command);
+    $rows = $sandbox->drush()->evaluate(function ($roles) {
+      // Load all of Drupal's permissions so that we have access to the
+      // "restrict access" property.
+      $all_permissions = \Drupal::service('user.permissions')->getPermissions();
 
-    $rows = [];
-    foreach ($untrusted_roles as $role) {
-      $untrusted_permissions = [];
-      // Get all permissions assigned to the untrusted role.
-      $role_permissions = $sandbox->drush(['format' => 'json'])
-        ->roleList($role);
-      
-      // Check each permission assigned to the untrusted role and determine if
-      // it is administrative.
-      // Administrative permissions will either have the "restrict access"
-      // property set, or the permission name contains the string "administer".
-      foreach ($role_permissions as $permission_name => $array) {
-        if ( isset($all_permissions[$permission_name]['restrict access']) ||
-          strstr($permission_name, 'administer') !== FALSE) {
-          $untrusted_permissions[] = $all_permissions[$permission_name]['title'];
+      $rows = [];
+      foreach ($roles as $role) {
+        $untrusted_permissions = [];
+
+        // Get all permissions assigned to the untrusted role.
+        $roleObj = \Drupal\user\Entity\Role::load($role);
+        $permissions = $roleObj->getPermissions();
+
+        // Check each permission assigned to the untrusted role and determine if
+        // it is administrative.
+        // Administrative permissions will either have the "restrict access"
+        // property set, or the permission name contains the string "administer".
+        foreach ($permissions as $permission) {
+          if (isset($all_permissions[$permission]['restrict access']) ||
+            strstr($permission, 'administer') !== FALSE ) {
+              $untrusted_permissions[] = $all_permissions[$permission]['title'];
+          }
+        }
+
+        if (!empty($untrusted_permissions)) {
+          $rows[] = [
+            'role' => $role,
+            'permissions' => implode(', ', $untrusted_permissions),
+          ];
         }
       }
-
-      if (!empty($untrusted_permissions)) {
-        $rows[] = [
-          'role' => $role,
-          'permissions' => implode(', ', $untrusted_permissions),
-        ];
-      }
-    }
+      return $rows;
+    }, [
+      'roles' => $sandbox->getParameter('untrusted_roles', ['anonymous', 'authenticated'])
+    ]);
 
     $sandbox->setParameter('rows', $rows);
 
